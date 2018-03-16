2ba29e46bcfcc9b79a8dc2dc4f72ce88be24aacf
Karen G <kgraves0701@gmail.com>
2018-03-16 00:25:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
     new_string << word.reverse + " "
-    #string = word.join(" ")
   end
   new_string.strip
 end
